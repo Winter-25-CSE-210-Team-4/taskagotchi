5754e48576ff1e1ed376319fcc65333c91ef48e8
sirajpal <sirajpal@ucsd.edu>
2025-02-11 14:55:17 -0800

    Updated React version, fixed issues with Chakra UI in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from "@chakra-ui/react"
-import React from "react"
-import ReactDOM from "react-dom/client"
+// import React from "react"
+// import ReactDOM from "react-dom/client"
 import './index.css'
 import App from './App.jsx'
+import { system } from "@chakra-ui/react/preset";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ChakraProvider>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ChakraProvider value={system}>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
