c5bed8ea1205cde9e6ba2581248d16da79cb770c
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2025-02-13 19:58:16 -0800

    removed old chakra components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css';
 
 function App() {
+  const handleClick = () => {
+    console.log('Button clicked!');
+  };
   return (
     <>
-      // TODO: Replace with page routing layout
+      {/* TODO: Replace with page routing layout */}
       <h1 className='text-5xl font-bold underline'>Hello world!</h1>
-      <button className='btn m-2'>Button</button>
+      <button className='btn m-2' onClick={handleClick}>
+        Button
+      </button>
     </>
   );
 }
