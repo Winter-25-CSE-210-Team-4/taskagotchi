9eadab1f1f72455f9680700f2c0bb7f8762bf241
ShenyiHuang <moucangcang@gmail.com>
2025-02-17 19:22:00 -0800

    add fonts and color palette

--- frontend/src/components/ui/Header.jsx
+++ frontend/src/components/ui/Header.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="bg-lightmint py-4 px-6 flex justify-between items-center w-full">
+    <div className="bg-secondary py-4 px-6 flex justify-between items-center w-full">
       <div className="flex items-center px-4">
         {/* <img src="/logo.png" alt="Logo" className="h-8 w-8 mr-2" /> */}
-        <h1 className="text-lg font-bold">Taskagotchi</h1>
+        <h1 className="text-4xl font-bold font-nanum">Taskagotchi</h1>
       </div>
       <div className="text-sm text-gray-700 hover:text-gray-900">CN</div>
     </div>
