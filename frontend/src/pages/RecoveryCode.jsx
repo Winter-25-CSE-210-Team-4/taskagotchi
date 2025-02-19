3938fb5ca1db1f098f6c86caad6d78e5ae62451a
Ayla Kurdak <aylakurdak@Aylas-MacBook-Pro.local>
2025-02-18 21:04:40 -0800

    Updated RecoveryCodePage to validate input.

--- frontend/src/pages/RecoveryCode.jsx
+++ frontend/src/pages/RecoveryCode.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import React from "react";
 import Header from "../components/ui/Header";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 /*
  * This page handles recovering a forgotten password by entering a random code emailed to the user
@@ -13,43 +13,56 @@ import { Link } from "react-router-dom";
  * This page also requires context info from the login page to get the email and recovery code
  */
 function RecoveryCodePage() {
-    return (
-        <div className="flex flex-col h-screen w-full min-w-[1024px]">
-          {/* Header stays at the top */}
-          <Header />
-    
-          {/* Main content area */}
-          <div className="flex flex-1 justify-center items-center bg-white">
-            {/* Login box */}
-            <div className="flex flex-col bg-mint/10 py-8 px-6 rounded-lg shadow-md w-full max-w-md">
-                <h2 className="text-2xl font-semibold text-center my-6">A recovery code has been sent to your email</h2>                
 
-                {/* Login form */}
-                <form>
-                <div>
-                    <input
-                    type="code"
-                    name="code"
-                    className="w-full p-2 mb-2 border border-black rounded bg-white"
-                    placeholder="Enter code"
-                    />
-                </div>
-                </form>
-                
-                <div className="w-full mt-4 justify-center items-center px-5 bg-darkmint text-center text-white border border-darkmint py-2 rounded hover:bg-mint">
-                    <button
-                    type="submit"
-                    className="w-full"
-                    onClick={()=>{window.location.href = "/reset"}}>
-                    Submit
-                    </button>
-                </div>
-                
-            </div>
-            </div>
-        </div>
-        );
+	const navigate = useNavigate()
+
+	const location = useLocation();
+	const { email, recoveryCode } = location.state || {};
+
+	const [inputCode, setInputCode] = useState(-1);
+	const [showError, setError] = useState(false);
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		if (inputCode !== recoveryCode) {
+			setError(true);
+			return;
+		}
+		navigate('/reset')
+	}
+
+
+	return (
+		<div className="flex flex-col h-screen w-full min-w-[1024px] bg-white">
+			<Header/>
+			<form onSubmit={handleSubmit} className='flex justify-center'>
+				<div className='w-[32rem] flex flex-col py-8 px-6 rounded-lg shadow-md w-full max-w-md'>
+					<p
+						className='py-2'>
+						A recovery code has been sent to {email}. Really, it's just in the console.
+					</p>
+					<div
+						className='flex flex-row gap-2'>
+						<input
+							type='number'
+							id='code'
+							className='w-1/2 p-2 mb-2 border border-black rounded bg-white'
+							placeholder='Enter recovery code'
+							onChange={(e)=>setInputCode(Number(e.target.value))}>
+						</input>
+						
+						<button
+							type='submit'
+							className='btn btn-primary flex flex-row items-center'>
+								Submit
+						</button>
+					</div>
+					{showError && <p className="text-red-500">Incorrect code</p>}
+				</div>
+			</form>
+		</div>
+	);
 };
-    
+	
 export default RecoveryCodePage;
-    
+	
