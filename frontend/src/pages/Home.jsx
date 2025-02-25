3345ed501931474dbaf45863fdf62c7bec9815a4
sirajpal <sirajpal@ucsd.edu>
2025-02-25 14:25:52 -0800

    minor changes to sidebar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,22 +15,23 @@ const HomePage = () => {
 
             {/* Main content */}
             <div className="flex flex-1 bg-white font-inter">
+
                 {/* Sidebar*/}
-                <div className="w-1/4 min-w-[250px] bg-gray p-4 flex flex-col border-r border-gray-300">
+                <div className="w-1/4 min-w-[250px] bg-zinc-100 p-4 flex flex-col border-r-4 border-zinc-200">
                 
-                    <h3 className="text-lg font-bold mb-2">Tasks</h3>
+                    <h3 className="text-base font-bold mb-2 pb-2 pr-2">Tasks</h3>
                     <ul className="mb-4">
                         <li className="flex justify-between text-sm">💧 Drink water <span>5:00pm</span></li>
                         <li className="flex justify-between text-sm">🚶 Take a walk <span>6:00pm</span></li>
                     </ul>
-                    <h3 className="text-lg font-bold mb-2">Goals</h3>
+                    <h3 className="text-base font-bold pb-2 pr-2 mb-2">Goals</h3>
                     <ul>
                         <li className="text-sm">💪 Be healthier</li>
                         <li className="text-sm">🌲 Go outdoors more!</li>
                     </ul>
                     <div className="mt-auto flex flex-col gap-2">
-                        <button className="btn btn-link text-primary">+ Add Task</button>
-                        <button className="btn btn-link text-primary">+ Add Goal</button>
+                        <button className="btn btn-link text-accent">+ Add Task</button>
+                        <button className="btn btn-link text-accent">+ Add Goal</button>
                     </div>
                 
                 </div>
