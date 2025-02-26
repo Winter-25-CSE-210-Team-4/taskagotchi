08b72d1c03e65d1e8d28694d54d4600fc3ca4989
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2025-02-26 13:39:53 -0800

    wip connecting backend

--- frontend/auth/hooks/useAxiosPrivate.js
+++ frontend/auth/hooks/useAxiosPrivate.js
@@ -1,10 +1,8 @@
 import { axiosPrivate } from '../../api/axios';
 import { useEffect } from 'react';
-import useRefreshToken from './useRefreshToken';
 import useAuth from './useAuth';
 
 const useAxiosPrivate = () => {
-  const refresh = useRefreshToken();
   const { auth } = useAuth();
 
   // interceptors to refresh token and add access token to header
@@ -16,30 +14,30 @@ const useAxiosPrivate = () => {
         }
         return config;
       },
-      (error) => Promise.reject(error),
+      (error) => Promise.reject(error)
     );
 
-    const responseIntercept = axiosPrivate.interceptors.response.use(
-      (response) => response,
-      async (error) => {
-        const prevRequest = error?.config;
-        if (error?.response?.status === 403 && !prevRequest?.sent) {
-          prevRequest.sent = true;
-          const newAccessToken = await refresh();
-          prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
-          return axiosPrivate(prevRequest);
-        }
-        // return error
-        return Promise.reject(error);
-      },
-    );
+    // const responseIntercept = axiosPrivate.interceptors.response.use(
+    //   (response) => response,
+    //   async (error) => {
+    //     const prevRequest = error?.config;
+    //     if (error?.response?.status === 403 && !prevRequest?.sent) {
+    //       prevRequest.sent = true;
+    //       // const newAccessToken = await refresh();
+    //         prevRequest.headers['Authorization'] = `Bearer ${newAccessToken}`;
+    //         return axiosPrivate(prevRequest);
+    //     }
+    //     // return error
+    //     return Promise.reject(error);
+    //   }
+    // );
 
     // Clean up for interceptors
     return () => {
       axiosPrivate.interceptors.request.eject(requestIntercept);
-      axiosPrivate.interceptors.response.eject(responseIntercept);
+      // axiosPrivate.interceptors.response.eject(responseIntercept);
     };
-  }, [auth, refresh]);
+  }, [auth]);
 
   return axiosPrivate;
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,26 @@
+import Header from '../components/ui/Header';
 const HomePage = () => {
+  return (
+    <>
+      <Header />
+      {/* Add a link to the login page */}
+      <div className='flex w-full mt-4 justify-center items-center px-5'>
+        <div className='w-1/3 bg-white text-center text-accent border border-accent border-2 py-2 rounded-lg hover:bg-secondary'>
+          <a href='/login' className='text-accent'>
+            Go to Login
+          </a>
+        </div>
+      </div>
+      {/* Add a link to the sign up page */}
 
-<h2 className="text-2xl font-semibold text-center my-6">Homeapge</h2>
-    
+      <div className='flex w-full mt-4 justify-center items-center px-5'>
+        <div className='w-1/3 bg-accent text-center text-white border border-accent py-2 rounded-lg hover:bg-primary hover:border-primary'>
+          <a href='/signup' className='text-white'>
+            Go to Sign up
+          </a>
+        </div>
+      </div>
+    </>
+  );
 };
 export default HomePage;
