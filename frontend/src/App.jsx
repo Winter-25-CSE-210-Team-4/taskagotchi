d5e43787007d5d1b32549bd6865c7070b6eb7c2f
sirajpal <sirajpal@ucsd.edu>
2025-02-27 21:21:41 -0800

    fixed error in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import LoginPage from "./pages/Login"; // Import your LoginPage
 import SignupPage from "./pages/Signup";
 import Header from "./components/ui/Header";
 import HomePage from "./pages/Home";
-import DummyPage from "./pages/Dummy";
 
 function App() {
 
@@ -42,8 +41,6 @@ function App() {
         <Route path="/signup" element={<SignupPage />} />
         {/* Home Page Route */}
         <Route path="/home" element={<HomePage />} />
-        {/* Dummy Page Route */}
-        <Route path="/dummy" element={<DummyPage />} />
       </Routes>
     </Router>
   );
