9d2e9f31a291c7c21a1a004740580e2706687106
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2025-02-28 12:33:02 -0800

    fixed signup redirect to login bug, renamed buttons

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -22,9 +22,7 @@ const HomePage = () => {
           className='w-1/3 bg-white text-center text-accent border border-accent border-2 py-2 rounded-lg hover:bg-secondary'
           onClick={() => navigate('/login')}
         >
-          <a href='/login' className='text-accent'>
-            Go to Login
-          </a>
+          <div className='text-accent'>Login</div>
         </div>
       </div>
       {/* Add a link to the sign up page */}
@@ -32,11 +30,9 @@ const HomePage = () => {
       <div className='flex w-full mt-4 justify-center items-center px-5'>
         <div
           className='w-1/3 bg-accent text-center text-white border border-accent py-2 rounded-lg hover:bg-primary hover:border-primary'
-          onClick={() => navigate('/login')}
+          onClick={() => navigate('/signup')}
         >
-          <a href='/signup' className='text-white'>
-            Go to Sign up
-          </a>
+          <div className='text-white'>Sign up</div>
         </div>
       </div>
     </>
