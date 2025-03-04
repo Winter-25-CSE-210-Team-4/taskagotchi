7df32c7ca3ac256c47643291bc4084b86854b125
Jophy <qloikfree@gmail.com>
2025-03-04 00:16:18 -0800

    Update task routes

--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -4,9 +4,9 @@ import { completeTask, deleteCompletedTasks } from "../controllers/taskControlle
 const router = express.Router();
 
 // mark task as complete
-router.put("/:task_id/complete", completeTask);
+// router.put("/:task_id/complete", completeTask);
 
 // delete completed tasks
 router.delete("/completed", deleteCompletedTasks);
-
+router.patch('/:task_id/complete', completeTask);
 export default router;
