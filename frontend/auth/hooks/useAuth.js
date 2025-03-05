863e5dc49ed33154f12053f3a360e9972dd0f6dd
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2025-03-05 13:54:02 -0800

    fixed bug with token not being read into context

--- frontend/auth/hooks/useAuth.js
+++ frontend/auth/hooks/useAuth.js
@@ -2,12 +2,14 @@ import { useContext, useEffect } from 'react';
 import { AuthContext } from '../AuthContextProvider';
 
 const useAuth = () => {
-  const { setUser, setLoggedIn } = useContext(AuthContext);
+  const { setUser, setLoggedIn, setAuth } = useContext(AuthContext);
   useEffect(() => {
     // Check for stored auth data on hook initialization
     const storedUser = localStorage.getItem('user');
+    const storedToken = localStorage.getItem('token');
     if (storedUser) {
       setUser(JSON.parse(storedUser));
+      setAuth({ accessToken: storedToken });
       setLoggedIn(true);
     }
   }, []);
