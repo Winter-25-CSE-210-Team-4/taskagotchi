52fc0dcdbe3058ec09fc644468674a75361361c5
sirajpal <sirajpal@ucsd.edu>
2025-03-06 13:26:17 -0800

    minor design changes

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -13,22 +13,21 @@ const LandingPage = () => {
                     backgroundPosition: 'center',
                 }}>
             
-                <div className='absolute inset-0 bg-white/20 backdrop-blur'></div>
+                <div className='absolute inset-0 backdrop-blur-sm'></div>
                 <div className='hero-overlay bg-transparent'></div>
                 <div className='hero-content text-neutral-content text-center bg-[#f1fbfa] p-8 rounded-xl'>
                     <div className="max-w-md">
                         <h1 className='mb-2 text-8xl font-bold font-nanum'>Taskagotchi</h1>
 
-                        <div className='flex flex-col flex-1 justify-center items-left mb-10'> 
-                            <h3 className='mb-5 text-2xl font-bold'>Get Started Below</h3>
+                        <div className='flex flex-col flex-1 justify-center items-left mb-4'> 
                             <button 
                             onClick={() => navigate('/login')}
-                            className="btn btn-accent mb-5 shadow-lg rounded-xl border-black bg-accent text-center text-white border border-accent py-2 rounded-lg hover:bg-primary hover:border-primary"
+                            className="btn btn-accent shadow-lg rounded-xl border-black bg-accent text-center text-xl text-white border border-accent mb-5 rounded-lg hover:bg-primary hover:border-primary"
                             >Login
                             </button>
                             <button 
                             onClick={() => navigate('/signup')}
-                            className="btn btn-outline bg-white text-center text-accent border-2 py-2 rounded-lg hover:bg-primary"
+                            className="btn btn-outline bg-transparent text-center text-xl text-accent border-2 rounded-lg hover:bg-primary"
                             >SignUp
                             </button>
                         </div>
