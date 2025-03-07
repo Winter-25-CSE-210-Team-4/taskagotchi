b53f724ef33e874969ff17444ab0f85c957fff8a
ziruichen <zirui.chen2020@gmail.com>
2025-03-07 07:25:50 -0800

    rollback taskRoutes.ts, change put to patch

--- backend/src/routes/taskRoutes.ts
+++ backend/src/routes/taskRoutes.ts
@@ -24,7 +24,7 @@ router.get("/:id", getTaskById);
 router.get("/user/:user_id", getTasksByUserId);
 
 // Update task
-router.put("/:id", updateTask);
+router.patch("/:id", updateTask);
 
 // Delete completed tasks
 router.delete("/completed", deleteCompletedTasks);
